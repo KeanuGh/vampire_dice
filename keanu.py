1677edb2b8553de2303e4c2027150799cb2a87f0
keanu <keanu@hotmail.co.uk>
2020-05-03 18:59:16 +0100

    Keanu made his attempt

--- keanu.py
+++ keanu.py
@@ -1,8 +1,32 @@
 import numpy as np
+from collections import Counter
 
-def odds(n_dice, n_itter, diff, ):
-    for i in n_itter:
-        for d in n_dice:
-            success = 0
-            dice_n = np.random.randint(1, 10)
-            if dice_n == 1
+
+def odds(n_dice, n_iter, diff, die_size=10):
+    successes = []
+    for i in range(n_iter):
+        success = 0
+
+        for d in range(n_dice):
+            dice_n = np.random.randint(die_size)
+            if dice_n == 0:
+                success += -1
+            elif dice_n == die_size - 1:
+                success += 2
+            elif dice_n >= diff:
+                success += 1
+
+        successes.append(success)
+
+    counts = Counter(successes)
+    counts_proba = {k: v / n_iter for k, v in counts.items()}
+    for key, value in sorted(counts_proba.items()):
+        print(f'{key}: {value}')
+
+
+if __name__ == '__main__':
+    for n in range(1):
+        odds(n_dice=18,
+             n_iter=1000000,
+             diff=7)
+        print('\n')
