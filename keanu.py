2ecfe28afd274bbc34bfb5ec97941085ca524eed
keanu <keanu@hotmail.co.uk>
2020-05-03 19:49:50 +0100

    fixed difficulty bug

--- keanu.py
+++ keanu.py
@@ -1,5 +1,6 @@
 import numpy as np
 from collections import Counter
+import matplotlib.pyplot as plt
 
 
 def odds(n_dice: int, n_iter: int, diff: int, die_size: int = 10) -> None:
@@ -30,11 +31,12 @@ def odds(n_dice: int, n_iter: int, diff: int, die_size: int = 10) -> None:
     counts_proba = {k: 100 * v / n_iter for k, v in counts.items()}
     for key, value in sorted(counts_proba.items()):
         print(f'{key}: {value}')
+    plt.bar(counts_proba.keys(), counts_proba.values())
+    plt.show()
 
 
 if __name__ == '__main__':
-    for n in range(10):
+    for n in range(1):
         odds(n_dice=3,
              n_iter=1000000,
              diff=7)
-        print('\n')
